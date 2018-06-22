5a0c5ed6d9559b3d8ac505f9a4106ef8b5beb9e7
Your Name <you@example.com>
2018-06-22 20:26:50 +0700

    tests

--- tests/main.c
+++ tests/main.c
@@ -3,6 +3,25 @@
 #include <Nachalo.c>
 #include <Vvod.c>
 #include <main.c>
+#include <ctest.h>
+
+CTEST(Menu_move, Correct) 
+{
+	int result = checkmenu(20);
+	int expected = 1;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Menu_move, Incorrect) 
+{
+    	int result = input_check(10); 
+	int expected = 0; 
+	ASSERT_EQUAL(expected, result);
+}
+
+int main(int argc, const char** argv) {
+    return ctest_main(argc, argv);
+}
 
 
 
