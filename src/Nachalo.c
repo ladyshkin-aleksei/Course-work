feaf4ce8d6db50da038799fec2a653151278783c
Your Name <you@example.com>
2018-06-22 15:33:40 +0700

    Start

--- src/Nachalo.c
+++ src/Nachalo.c
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <math.h>
+//#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctime>
-
-
+//#include <stdlib.h>
+//#include <ctime>
 
 using namespace std;
 
@@ -12,13 +10,10 @@ void nachalo()
 {		
 	cout << "Правила игры: " << endl;
 	cout << endl;
-	cout << endl;
 	cout << "Добро пожаловать в игру <Виселица>! Тебе нужно будет угадать загаданое мною слово, используя английские буквы на своей клавиатуре. Если в слове не будет твоей буквы, то количество жизней, которых 6 штук, будут уменьшаться. Если угадаешь моё слово, то ты выйграл, если нет, то можешь начать занаво. Удачи тебе!! " << endl;
 	cout << endl;
-	cout << endl;
 	cout << "Буквы, которые можно использовать: " << endl;
-	printf ("a b c d e f g h i j k l m n o p q r s t u v w x y z");
-	cout << endl;
+	cout <<("a b c d e f g h i j k l m n o p q r s t u v w x y z")<< endl;
 	cout << endl;
 	cout << "Игра началась!" << endl;
 	cout << endl;
