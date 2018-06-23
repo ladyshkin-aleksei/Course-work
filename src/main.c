a4b6c8e1e8b9adc6e3ca025e30b5212bb71d1bf9
Your Name <you@example.com>
2018-06-23 18:12:43 +0700

    basis

--- src/main.c
+++ src/main.c
@@ -8,35 +8,35 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
-    int N=0;
+	setlocale(LC_ALL, "rus");
+	int N=0;
 	
-    while (N != 1 || N != 0)
-    {
+	while (N != 1 || N != 0)
+	{
 
-        cout << '\n';
-        cout << "Начать игру: 1" << '\n';
-        cout << "Выход: 0" << '\n';
-        cin >> N; 
+		cout << '\n';
+		cout << "Начать игру: 1" << '\n';
+		cout << "Выход: 0" << '\n';
+		cin >> N; 
 	    
-        if (N == 0){
-            system ("clear");
-            cout << "Игра окончена" << '\n';
-            break;
-            } 
+	if (N == 0){
+		system ("clear");
+		cout << "Игра окончена" << '\n';
+		break;
+		} 
 	    
-        if (N == 1){
-            system ("clear");
-            nachalo();
-            vvod();
-            continue;	
-            }
+	if (N == 1){
+		system ("clear");
+		nachalo();
+		vvod();
+		continue;	
+		}
 	    
-        if (N !=1 && N !=0){
-            cout << '\n';
-            printf("Не верный режим");
-            cout << '\n';
-            continue;
-            }
-    }	
+	if (N !=1 && N !=0){
+		cout << '\n';
+		printf("Не верный режим");
+		cout << '\n';
+		continue;
+		}
+	}	
 }
